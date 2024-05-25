669896038cd94fcbe8246d533a1d0193b53185b8
Irene Spanou <2804507+iresil@users.noreply.github.com>
2024-05-25 16:44:06 +0300

    Logged OpenCSV automated string conversions

--- src/main/java/FowlFlightForensics/util/string/UppercaseConverter.java
+++ src/main/java/FowlFlightForensics/util/string/UppercaseConverter.java
@@ -1,13 +1,21 @@
 package FowlFlightForensics.util.string;
 
 import com.opencsv.bean.AbstractBeanField;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class UppercaseConverter extends AbstractBeanField<String, String> {
+    private final Logger logger = LogManager.getLogger(getClass());
+
     @Override
     protected String convert(String value) {
+        String modified = null;
         if (value != null) {
-            return value.toUpperCase();
+            modified = value.toUpperCase();
+            if (!value.equals(modified)) {
+                logger.info("Converted {} value from {} to {}.", field.getName(), value, modified);
+            }
         }
-        return null;
+        return modified;
     }
 }
