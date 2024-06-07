10c49d8d7dd8a108b7db504e8ed3279919813bf4
Irene Spanou <2804507+iresil@users.noreply.github.com>
2024-06-07 20:27:54 +0300

    Refactored DataWiperService

--- src/main/java/FowlFlightForensics/service/DataWiperService.java
+++ src/main/java/FowlFlightForensics/service/DataWiperService.java
@@ -1,11 +1,23 @@
 package FowlFlightForensics.service;
 
+import FowlFlightForensics.domain.IncidentKey;
 import FowlFlightForensics.util.BaseComponent;
+import FowlFlightForensics.util.serdes.JsonKeySerde;
 import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,8 +28,31 @@ public class DataWiperService extends BaseComponent {
     @Autowired
     private AdminClient adminClient;
 
+    @Value("${app.kafka.topics.grouped}")
+    private String groupedDataTopic;
+
+    @Autowired
+    StreamsBuilder builder;
+
     public void delete() {
-        Map<TopicPartition, RecordsToDelete> deleteMap = new HashMap<>();
+        deleteLocalStateStore();
+
+        Map<TopicPartition, RecordsToDelete> partitionsForCleanup = getPartitionsForCleanup();
+        deleteMessages(partitionsForCleanup);
+    }
+
+    private void deleteLocalStateStore() {
+        try {
+            FileSystemUtils.deleteRecursively(Paths.get(Paths.get(".").normalize().toAbsolutePath().toString(),
+                    "kafka-streams"));
+            logger.info("Recursive cleanup of kafka streams directory successfully completed.");
+        } catch (IOException e) {
+            logger.error("Couldn't clean kafka streams directory.");
+        }
+    }
+
+    private Map<TopicPartition, RecordsToDelete> getPartitionsForCleanup() {
+        Map<TopicPartition, RecordsToDelete> result = new HashMap<>();
         try {
             adminClient.listTopics().names().get().forEach(topic -> {
                 if (!topic.contains("STATE-STORE")) {
@@ -41,7 +76,7 @@ public class DataWiperService extends BaseComponent {
 
                         for (Map.Entry<TopicPartition, ListOffsetsResult.ListOffsetsResultInfo> entry : offsets.entrySet()) {
                             long lowWatermark = offsets.get(entry.getKey()).offset();
-                            deleteMap.put(new TopicPartition(topic, partitionIndex), RecordsToDelete.beforeOffset(lowWatermark));
+                            result.put(new TopicPartition(topic, partitionIndex), RecordsToDelete.beforeOffset(lowWatermark));
                         }
                     }
                 }
@@ -49,15 +84,37 @@ public class DataWiperService extends BaseComponent {
         } catch (InterruptedException | ExecutionException e) {
             logger.error("Couldn't prepare topic deletion.", e);
         }
+        return result;
+    }
 
-        DeleteRecordsResult result = adminClient.deleteRecords(deleteMap);
+    private void deleteMessages(Map<TopicPartition, RecordsToDelete> partitionsForCleanup) {
+        DeleteRecordsResult result = adminClient.deleteRecords(partitionsForCleanup);
         result.lowWatermarks().forEach((tp, lowWatermark) ->
         {
             try {
                 logger.info("Data deletion for topic {} completed. Low watermark for partition {}: {}", tp.topic(), tp.partition(), lowWatermark.get().lowWatermark());
             } catch (InterruptedException | ExecutionException e) {
-                logger.error("Couldn't confirm topic data deletion.");
+                logger.error("Couldn't confirm topic data deletion.", e);
             }
         });
+    }
+
+    @Deprecated
+    private void streamCleanup() {
+        JsonKeySerde keySerde = new JsonKeySerde();
+
+        KStream<IncidentKey, Long> rawIncidentStream = builder.stream(groupedDataTopic, Consumed.with(keySerde, Serdes.Long()));
+        rawIncidentStream.map((k, v) -> new KeyValue<>(k, 0L))
+                .groupBy((k, v) -> k, Grouped.with(keySerde, Serdes.Long()))
+                .reduce(Long::sum,
+                        Materialized.<IncidentKey, Long, KeyValueStore<Bytes, byte[]>> as("AGGREGATES-STATE-STORE")
+                        .withKeySerde(keySerde)
+                        .withValueSerde(Serdes.Long())
+                        //.withStoreType(Materialized.StoreType.IN_MEMORY)
+                        //.withRetention(Duration.ofSeconds(1L))
+                        //.withCachingDisabled()
+                        )
+                .toStream()
+                .to(groupedDataTopic);
     }
 }
