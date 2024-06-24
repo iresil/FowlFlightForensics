88538a8cf48a8fcb76225f4f099dfb31398dc4b1
Irene Spanou <2804507+iresil@users.noreply.github.com>
2024-06-24 16:35:33 +0300

    Added basic kafka integration test

--- src/main/java/FowlFlightForensics/domain/dto/IncidentSummary.java
+++ src/main/java/FowlFlightForensics/domain/dto/IncidentSummary.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
 /**
  * An {@code IncidentSummary} object describes a shortened version of the {@code IncidentDetails} object, from which
@@ -71,7 +72,7 @@ public class IncidentSummary extends BaseComponent {
     /**
      * Method used for logging purposes, in places where we need to know the internal contents of the {@code IncidentSummary}
      * object.
-     * @return A {@code String} representation of the original IncidentSummary object.
+     * @return A {@code String} representation of the original {@code IncidentSummary} object.
      */
     @Override
     public String toString() {
@@ -88,5 +89,56 @@ public class IncidentSummary extends BaseComponent {
         result.delete(result.length() - 2, result.length() - 1);
         result.append(" }");
         return result.toString();
+    }
+
+    /**
+     * Overrides the default {@code equals}, to be used for testing.
+     * @param o The object to compare to.
+     * @return {@code true} if objects are equal, {@code false} otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof IncidentSummary)) {
+            return false;
+        }
+
+        IncidentSummary c = (IncidentSummary) o;
+
+        return Objects.equals(recordId, c.recordId)
+                && Objects.equals(year, c.year)
+                && Objects.equals(month, c.month)
+                && Objects.equals(day, c.day)
+                && Objects.equals(aircraft, c.aircraft)
+                && Objects.equals(aircraftMass, c.aircraftMass)
+                && Objects.equals(engines, c.engines)
+                && Objects.equals(airportId, c.airportId)
+                && Objects.equals(airportName, c.airportName)
+                && Objects.equals(state, c.state)
+                && Objects.equals(faaRegion, c.faaRegion)
+                && Objects.equals(warningIssued, c.warningIssued)
+                && Objects.equals(flightPhase, c.flightPhase)
+                && Objects.equals(speciesId, c.speciesId)
+                && Objects.equals(speciesName, c.speciesName)
+                && Objects.equals(speciesQuantityMin, c.speciesQuantityMin)
+                && Objects.equals(speciesQuantityMax, c.speciesQuantityMax)
+                && Objects.equals(fatalities, c.fatalities)
+                && Objects.equals(injuries, c.injuries)
+                && Objects.equals(aircraftDamage, c.aircraftDamage);
+    }
+
+    /**
+     * Overrides the default {@code hashCode} method, since {@code equals} has also been overridden, to ensure the same
+     * objects don't get different hash values.
+     * @return The {@code recordId} as a unique hash code.
+     */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(recordId, year, month, day, aircraft, airportId, airportName, state, flightPhase, speciesId,
+                speciesName, aircraftDamage);
     }
 }
---
+++ src/test/java/FowlFlightForensics/KafkaTests.java
@@ -0,0 +1,76 @@
+package FowlFlightForensics;
+
+import FowlFlightForensics.domain.dto.IncidentKey;
+import FowlFlightForensics.domain.dto.IncidentSummary;
+import FowlFlightForensics.service.DataWiperService;
+import FowlFlightForensics.util.serdes.JsonKeySerde;
+import FowlFlightForensics.util.serdes.JsonValueSerde;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.security.RunAs;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunAs("KafkaTests")
+@SpringBootTest
+@EmbeddedKafka(topics = { "raw-data-topic-test" })
+public class KafkaTests {
+    private static final String TEST_TOPIC = "raw-data-topic-test";
+
+    private JsonKeySerde keySerde = new JsonKeySerde();
+    private JsonValueSerde incidentSerde = new JsonValueSerde();
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Test
+    public void testKafkaIntegration() {
+        Consumer<IncidentKey, IncidentSummary> consumer = configureConsumer();
+        Producer<IncidentKey, IncidentSummary> producer = configureProducer();
+
+        IncidentSummary incidentSummary = new IncidentSummary(111, 2024, 06, 24, "T-38A",
+                null, null, "KIWA", "PHOENIX-MESA GATEWAY", "AZ", "AWP",
+                null, "CLIMB", "K5105", "MERLIN", 2, 10,
+                null, null, false);
+        producer.send(new ProducerRecord<>(TEST_TOPIC, incidentSummary.getKey(), incidentSummary));
+
+        ConsumerRecord<IncidentKey, IncidentSummary> singleRecord = KafkaTestUtils.getSingleRecord(consumer, TEST_TOPIC);
+        assertThat(singleRecord).isNotNull();
+        assertThat(singleRecord.key()).isEqualTo(incidentSummary.getKey());
+        assertThat(singleRecord.value()).isEqualTo(incidentSummary);
+
+        consumer.close();
+        producer.close();
+    }
+
+    private Consumer<IncidentKey, IncidentSummary> configureConsumer() {
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", embeddedKafkaBroker);
+        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        Consumer<IncidentKey, IncidentSummary> consumer = new DefaultKafkaConsumerFactory<IncidentKey, IncidentSummary>(consumerProps,
+                keySerde.deserializer(), incidentSerde.deserializer())
+                .createConsumer();
+        consumer.subscribe(Collections.singleton(TEST_TOPIC));
+        return consumer;
+    }
+
+    private Producer<IncidentKey, IncidentSummary> configureProducer() {
+        Map<String, Object> producerProps = new HashMap<>(KafkaTestUtils.producerProps(embeddedKafkaBroker));
+        return new DefaultKafkaProducerFactory<IncidentKey, IncidentSummary>(producerProps, keySerde.serializer(),
+                incidentSerde.serializer()).createProducer();
+    }
+}
